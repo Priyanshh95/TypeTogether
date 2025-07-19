7306b27afd80afb8dd9b74d1258b5ba11ab17f07
Priyanshh95 <priyanshub243@gmail.com>
2025-07-19 14:58:25 +0530

    connect Express backend to MongoDB using Mongoose and dotenv

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
 const express = require('express');
 const cors = require('cors');
 
